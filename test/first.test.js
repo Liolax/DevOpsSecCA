8be9fddfdf3ee39b7eb4ebb66de80c4de3f26edb
Liolax <donleonkot@gmail.com>
2025-03-16 04:11:00 +0000

    Fix test file to use ES modules and update Node.js version in CI workflow

--- test/first.test.js
+++ test/first.test.js
@@ -1,8 +1,7 @@
-const assert = require('assert');
+import assert from 'assert';
 
 describe('First Test Suite', function () {
   it('should pass a simple test', function () {
     assert.strictEqual(2 + 2, 4);
   });
 });
-
